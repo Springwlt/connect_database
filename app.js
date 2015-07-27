c706a21d0d4767aa713bc1778e06408b7eac56a6
wanglitao <wanglitao1202@outlook.com>
2015-07-27 21:31:45 +0800

    使用sequelize改写

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -11,52 +13,80 @@ app.use(express.static('public'));
 app.engine('.html',ejs.__express);
 app.set('view engine','html');
 
-app.get('/', function (req, res) {
-  res.render('index',{})
-});
+var Sequelize = require('sequelize')
+  , sequelize = new Sequelize('student', 'root', 'root', {
+      dialect: "mysql", 
+      port:    3306, 
 
-function createConnection() {
-   return mysql.createConnection({
-       host: 'localhost',
-       user: 'root',
-       password: 'root',
-       database: 'student',
-       port: 3306
-   });
-}
+})
 
-app.get('/student_names',function(req,res) {
-    var conn = createConnection();
-    conn.connect();
-    conn.query('select * from student_name', function(err, rows, fields) {
-        if (err) throw err;
-        //res.send(rows);
-        res.send({
-            state:'1',
-            data:rows,
-            information:""
-        })
+// var connectedTest = sequelize.authenticate()
+//     .then(function () {
+//         console.log("成功! ");
+//     })
+//     .catch(function (err) {
+//         console.log("错误");
+//     })
+//     .done();
+
+    var student_name = sequelize.define('student_name', {
+        name: Sequelize.STRING,
+    }, {
+        tableName: 'student_name',
+        timestamps: false
     });
-    conn.end();
-});
 
-app.delete('/student_name', function(req, res) {
-   var id = req.body.id;
-   var conn = createConnection();
-   conn.connect();
-   var deleteData = 'delete from student_name where id=' + id;
-   conn.query(deleteData, function(err, results) {
-   });
-   conn.end();
-});
+    function get_all_data() {
+        student_name.findAll().then(function(data) {
+            return data;
+        });
+    }
+
+    app.get('/', function(req, res) {
+        res.render('index', {});
+    });
+
+
+    app.get('/student_names', function(req, res) {
+        var result = [];
+        student_name.findAll().then(function(data) {
+            for (var i = 0; i < data.length; i++) {
+                result.push(data[i].dataValues);
+            }
+        }).then(function() {
+            res.send({
+                status: 1,
+                data: result,
+                message: ''
+            });
+        });
+    });
+
+    app.delete('/student_name', function(req, res) {
+        var id = req.params.id;
+        student_name.destroy({
+            where: {
+                id: id
+            }
+        }).done(function() {
+            res.send({
+                status: 1,
+                message: ''
+            });
+        });
+    });
+
 
 app.post('/table', function(req, res) {
    var name = req.body.name;
-   var conn = createConnection();
-   conn.connect();
-   conn.query('insert into student_name(name) values("'+name+'")', function(err, results) {
-   });
-   conn.end();
+    student_name.create({
+        name: name,
+    }).done(function() {
+        res.send({
+        status: 1,
+        message: ''
+        });
+    });
 });
 
 var server = app.listen(3000, function () {
